979a2dbe817cae5c2d9d4602d9c9f1e6dab4d950
Balazs Prehoda <prehoda.balazs@gmail.com>
2018-12-02 12:08:21 +0100

    Created transaction Compensate

--- temalab-rlr/lib/script.js
+++ temalab-rlr/lib/script.js
@@ -52,3 +52,43 @@ async function burn(tx) {
   await tokenRegistry.removeAll(tx.tokens);
   await vehicleRegistry.update(tx.vehicle);
 }
+
+/**
+ * Compensate transaction processor function.
+ * @param {hu.bme.mit.temalab.Compensate} tx The Compensate transaction instance.
+ * @transaction
+ */
+async function compensate(tx) {
+  let factory = getFactory();
+  let vehicleRegistry = await getParticipantRegistry('hu.bme.mit.temalab.Vehicle');
+  
+  const tokenRegistry = await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken');   
+    let existingTokens = await tokenRegistry.getAll();
+  
+    let numberOfTokens = 0;
+  	let tokenIds = new Array();
+    await existingTokens .forEach(function (token) {
+      numberOfTokens ++;
+      tokenIds.push(token.tokenId);
+    });
+
+  for (let i = 0; i < tx.amount; i++) {
+    let tokenId;
+    let j = 0;
+    while(tokenId == null) {
+      if (tokenIds.indexOf("token"+j) < 0) {
+        tokenId = "token"+j;
+        tokenIds.push(tokenId);
+      }
+      j++;
+    }
+    let token = factory.newResource('hu.bme.mit.temalab', 'PreemptionUtilityToken', tokenId);
+    await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken')
+      .then(function(tokenRegistry) {
+      return tokenRegistry.add(token);
+    });
+    tx.vehicle.tokens.push(token);
+  }
+  
+  vehicleRegistry.update(tx.vehicle);
+}
