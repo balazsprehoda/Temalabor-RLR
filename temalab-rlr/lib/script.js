70fd8dc823dc35e6ea00bd7916a6d9ecdc1bc7d3
Balazs Prehoda <prehoda.balazs@gmail.com>
2018-12-10 03:06:33 +0100

    DateTime handling added

--- temalab-rlr/lib/script.js
+++ temalab-rlr/lib/script.js
@@ -7,12 +7,12 @@ async function issue(tx) {
   let factory = getFactory();
   let vehicleRegistry = await getParticipantRegistry('hu.bme.mit.temalab.Vehicle');
   let tokenRegistry = await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken');
-  
+  try {
   // Check if the vehicle truly exists.
   let vehicleExists = await vehicleRegistry.exists(tx.vehicle.vehicleId);
-  if(!vehicleExists) {
-    return console.log("No vehicle exists with the given ID.");;
-  }
+  if(!vehicleExists) throw "No vehicle exists with the given ID.";
+  // DateTime sanity check. 10 minutes in millis.
+  if(new Date() - 10*60*1000 > tx.dateTime || new Date() < tx.dateTime) throw "Wrong date."
   let tokenArray = new Array();
   
   /* Generating unique token ids is the responsibility of the Traffic Authority. The script checks if the provided tokenId is truly unique. */
@@ -22,13 +22,15 @@ async function issue(tx) {
     if(!tokenExists) {
   	let token = await factory.newResource('hu.bme.mit.temalab', 'PreemptionUtilityToken', tx.tokenIds[i]);
     token.owner = tx.vehicle;
-    token.latestUpdateTime = new Date();
+    token.latestUpdateTime = tx.dateTime;
     tokenArray.push(token);
     }
   }
   await tokenRegistry.addAll(tokenArray);
   tx.vehicle.tokens = await tx.vehicle.tokens.concat(tokenArray);
   await vehicleRegistry.update(tx.vehicle);
+  }
+  catch(err) { console.log(err); }
 }
 
 /**
@@ -45,6 +47,8 @@ async function burn(tx) {
   
   try{
     if(!vehicleExists) throw "Vehicle with the given ID does not exist!";
+    // DateTime sanity check. 10 minutes in millis.
+  	if(new Date() - 10*60*1000 > tx.dateTime || new Date() < tx.dateTime) throw "Wrong date."
       for( let i = 0; i < tx.tokens.length; i++ ) {
         // Check if the given token truly exists.
         let tokenExists = await tokenRegistry.exists(tx.tokens[i].tokenId);
@@ -54,7 +58,7 @@ async function burn(tx) {
           tx.vehicle.tokens.splice(tx.vehicle.tokens.indexOf(tx.tokens[i]), 1);
           // and it becomes invalidated.
           tx.tokens[i].isValid = false;
-          tx.tokens[i].latestUpdateTime = new Date();
+          tx.tokens[i].latestUpdateTime = tx.dateTime;
           // Only those tokens will be updated, which really belong to the vehicle
           tokenArray.push(tx.tokens[i]);
         }
@@ -78,29 +82,36 @@ async function unBurn(tx) {
   let vehicleRegistry = await getParticipantRegistry('hu.bme.mit.temalab.Vehicle');
   let tokenRegistry = await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken');
   let vehicles = new Array();
+  try {
+    // DateTime sanity check. 10 minutes in millis.
+  	if(new Date() - 10*60*1000 > tx.dateTime || new Date() < tx.dateTime) throw "Wrong date."
   
-  for ( let i = 0; i < tx.tokens.length; i++) {
-    try{
-      let tokenExists = await tokenRegistry.exists(tx.tokens[i].tokenId);
-      if(!tokenExists) throw "Token with given ID does not exist.";
-      // If the TrafficAuthority signed the token, and the token is missing from the vehicle's array,
-      if(tx.tokens[i].trafficAuthoritySignature && tx.tokens[i].owner.tokens.indexOf(tx.tokens[i]) < 0) {
-        // give it to the array,
-        tx.tokens[i].owner.tokens.push(tx.tokens[i]);
-        // validate it,
-        tx.tokens[i].isValid = true;
-        // and remove the signature of the Traffic Authority.
-        tx.tokens[i].trafficAuthoritySignature = false;
-        tx.tokens[i].latestUpdateTime = new Date();
+    for ( let i = 0; i < tx.tokens.length; i++) {
+      try{
+        let tokenExists = await tokenRegistry.exists(tx.tokens[i].tokenId);
+        if(!tokenExists) throw "Token with given ID does not exist.";
+        // If the TrafficAuthority signed the token, and the token is missing from the vehicle's array,
+        if(tx.tokens[i].trafficAuthoritySignature && tx.tokens[i].owner.tokens.indexOf(tx.tokens[i]) < 0) {
+          // give it to the array,
+          tx.tokens[i].owner.tokens.push(tx.tokens[i]);
+          // validate it,
+          tx.tokens[i].isValid = true;
+          // and remove the signature of the Traffic Authority.
+          tx.tokens[i].trafficAuthoritySignature = false;
+          tx.tokens[i].latestUpdateTime = tx.dateTime;
 
-        /* Register the vehicles which will need to be updated. */
-        if(vehicles.indexOf(tx.tokens[i].owner) === -1)
-          vehicles.push(tx.tokens[i].owner);
+          /* Register the vehicles which will need to be updated. */
+          if(vehicles.indexOf(tx.tokens[i].owner) === -1)
+            vehicles.push(tx.tokens[i].owner);
+        }
+      }
+      catch(err){
+        console.log(err);
       }
     }
-    catch(err){
-      console.log(err);
-    }
+  }
+  catch(err){
+    console.log(err);
   }
   
   /* Updating the appropriate registries batch-like */
@@ -115,15 +126,14 @@ async function unBurn(tx) {
  */
 async function clear(tx) {
   let tokenRegistry = await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken');
-  
   /* Getting all invalidated and expired tokens */
   let existingTokens = await tokenRegistry.getAll();
   let invalidTokens = await existingTokens.filter( token => token.isValid === false);
-  
+
   /* 60 days in milliseconds */
   const EXPIRE_TIME_IN_MILLIS = 60*24*60*60*1000;
   let tokensToDelete = invalidTokens.filter( token => token.latestUpdateTime.getTime() < new Date().getTime() - EXPIRE_TIME_IN_MILLIS );
-  
+
   /* Delete the invalidated and expired tokens */
   tokenRegistry.removeAll(tokensToDelete);
 }
@@ -229,22 +239,28 @@ async function updateEntryPoints(tx) {
 async function sign(tx) {
   let tokenRegistry = await getAssetRegistry('hu.bme.mit.temalab.PreemptionUtilityToken');
   let tokenArray = new Array();
+  try {
+    // DateTime sanity check. 10 minutes in millis.
+  	if(new Date() - 10*60*1000 > tx.dateTime || new Date() < tx.dateTime) throw "Given date is too old."
   
-  /* Signing all the given tokens. */
-  for( let i = 0; i < tx.tokens.length; i++) {
-    let tokenExists = tokenRegistry.exists(tx.tokens[i].tokenId);
-    try {
-      if(!tokenExists) throw "Token with the given ID does not exist!";
-      if(!tx.tokens[i].isValid) {
-        tx.tokens[i].trafficAuthoritySignature = true;
-        tokenArray.push(tx.tokens[i]);
+    /* Signing all the given tokens. */
+    for( let i = 0; i < tx.tokens.length; i++) {
+      let tokenExists = tokenRegistry.exists(tx.tokens[i].tokenId);
+      try {
+        if(!tokenExists) throw "Token with the given ID does not exist!";
+        if(!tx.tokens[i].isValid) {
+          tx.tokens[i].trafficAuthoritySignature = true;
+          tx.tokens[i].latestUpdateTime = tx.dateTime;
+          tokenArray.push(tx.tokens[i]);
+        }
+      }
+      catch(err) {
+        console.log(err);
       }
     }
-    catch(err) {
-      console.log(err);
-    }
+
+    /* Updating the appropriate AssetRegistry batch-like. */
+    tokenRegistry.updateAll(tokenArray);
   }
-  
-  /* Updating the appropriate AssetRegistry batch-like. */
-  tokenRegistry.updateAll(tokenArray);
+  catch(err) { console.log(err); }
 }
